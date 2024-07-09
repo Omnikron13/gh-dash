c481b7b0aba180e3172205dc72e329ec8721fa92
Omnkiron13 <j.sabey@uea.ac.uk>
2024-07-09 19:58:45 +0100

    generate cpu.profile file

--- gh-dash.go
+++ gh-dash.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+   "os"
+   "log"
+   "runtime/pprof"
 	"github.com/dlvhdr/gh-dash/v4/cmd"
 )
 
 func main() {
+   f, err := os.Create("cpu.profile")
+   if err != nil {
+      log.Fatal("could not create CPU profile: ", err)
+   }
+   defer f.Close()
+   if err := pprof.StartCPUProfile(f); err != nil {
+      log.Fatal("could not start CPU profile: ", err)
+   }
+   defer pprof.StopCPUProfile()
+
 	cmd.Execute()
 }
